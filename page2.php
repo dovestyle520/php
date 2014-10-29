51304e5e98555683ef02ad6e97df147787292356
dovestyle520 <307908410@qq.com>
2014-10-29 23:09:10 +0800

    Revert "add old code"
    This reverts commit 6ef58ef787bb6f9bbef045d40a02083308cd4b52.

--- page2.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-
-    echo 'The content of $_SESSION[\'sess_var\'] is'.$_SESSION['sess_var'].'<br />';
-    unset($_SESSION['sess_var']);
-?>
-
-<a href="\test\page3.php">Next page</a>
